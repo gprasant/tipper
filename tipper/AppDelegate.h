295aa7e1a06f6d71957d1487646b3e7a394f6d99
Prasanth Guruprasad <nagu89@gmail.com>
2015-09-20 17:38:49 -0700

    finished req 1

---
+++ tipper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tipper
+//
+//  Created by Prasanth Guruprasad on 9/20/15.
+//  Copyright (c) 2015 Prasanth Guruprasad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tipper/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipper
+//
+//  Created by Prasanth Guruprasad on 9/20/15.
+//  Copyright (c) 2015 Prasanth Guruprasad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
